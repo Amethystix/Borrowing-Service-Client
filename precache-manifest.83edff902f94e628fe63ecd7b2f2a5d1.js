ecf1af8f02927280df0db7f365e51b9f401c86a0
lad475 <lad475@nyu.edu>
2018-12-15 20:40:23 -0500

    deploy

---
+++ precache-manifest.83edff902f94e628fe63ecd7b2f2a5d1.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a2d961cdcb7523701f9",
+    "url": "/Borrowing-Service-Client/css/chunk-eff5b30e.6fc02657.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Borrowing-Service-Client/robots.txt"
+  },
+  {
+    "revision": "5a2d961cdcb7523701f9",
+    "url": "/Borrowing-Service-Client/js/chunk-eff5b30e.8324a545.js"
+  },
+  {
+    "revision": "8635ee912f73b35a3682",
+    "url": "/Borrowing-Service-Client/css/app.1f80a58d.css"
+  },
+  {
+    "revision": "d0b86aaa3c0a0b0734ad",
+    "url": "/Borrowing-Service-Client/css/chunk-11ccd78c.95d5bc1b.css"
+  },
+  {
+    "revision": "d0b86aaa3c0a0b0734ad",
+    "url": "/Borrowing-Service-Client/js/chunk-11ccd78c.9432758a.js"
+  },
+  {
+    "revision": "a14c7aa1630d54c46672",
+    "url": "/Borrowing-Service-Client/css/chunk-17cbe6ce.e8f7f582.css"
+  },
+  {
+    "revision": "a14c7aa1630d54c46672",
+    "url": "/Borrowing-Service-Client/js/chunk-17cbe6ce.bdfb529e.js"
+  },
+  {
+    "revision": "46286017b556cf219001",
+    "url": "/Borrowing-Service-Client/css/chunk-4d890ee2.081b04d0.css"
+  },
+  {
+    "revision": "46286017b556cf219001",
+    "url": "/Borrowing-Service-Client/js/chunk-4d890ee2.65b3bc27.js"
+  },
+  {
+    "revision": "a27a428c265502fa7f56",
+    "url": "/Borrowing-Service-Client/css/chunk-54f2bc06.f49ccbbe.css"
+  },
+  {
+    "revision": "a27a428c265502fa7f56",
+    "url": "/Borrowing-Service-Client/js/chunk-54f2bc06.8e8f288d.js"
+  },
+  {
+    "revision": "df0a829448771bda9f9c",
+    "url": "/Borrowing-Service-Client/css/chunk-5907760c.083dd205.css"
+  },
+  {
+    "revision": "df0a829448771bda9f9c",
+    "url": "/Borrowing-Service-Client/js/chunk-5907760c.f809553b.js"
+  },
+  {
+    "revision": "9ed9c9e6eb540cad099b",
+    "url": "/Borrowing-Service-Client/css/chunk-7c82ad4e.2f2568f7.css"
+  },
+  {
+    "revision": "9ed9c9e6eb540cad099b",
+    "url": "/Borrowing-Service-Client/js/chunk-7c82ad4e.1d0db9b3.js"
+  },
+  {
+    "revision": "b67a979490f83f0760cb",
+    "url": "/Borrowing-Service-Client/js/about.5926637a.js"
+  },
+  {
+    "revision": "8635ee912f73b35a3682",
+    "url": "/Borrowing-Service-Client/js/app.6fa965ae.js"
+  },
+  {
+    "revision": "e28bcabf075ee196b8ba",
+    "url": "/Borrowing-Service-Client/js/chunk-vendors.f31169c6.js"
+  },
+  {
+    "revision": "a50b5182da834f590acad6a0e5a9f85f",
+    "url": "/Borrowing-Service-Client/img/noImage.a50b5182.png"
+  },
+  {
+    "revision": "e9f1e60eceef9a732b2cd1bb83a9aa3d",
+    "url": "/Borrowing-Service-Client/img/PTSpinnerG.e9f1e60e.png"
+  },
+  {
+    "revision": "3263af2fbd63e0a999d4bd571a31e650",
+    "url": "/Borrowing-Service-Client/img/github-logo-white.3263af2f.svg"
+  },
+  {
+    "revision": "2c6e680bfb709e5e4b2265c84d61da76",
+    "url": "/Borrowing-Service-Client/img/facebook-logo-white.2c6e680b.svg"
+  },
+  {
+    "revision": "ca9151bdcec339658e02ae9b00f88221",
+    "url": "/Borrowing-Service-Client/img/facebook-logo.ca9151bd.svg"
+  },
+  {
+    "revision": "3ff240cc1704466d4ebeeb8d5e8b37bc",
+    "url": "/Borrowing-Service-Client/img/github-logo.3ff240cc.svg"
+  },
+  {
+    "revision": "9ddf64370868eb95d94ac71e47268780",
+    "url": "/Borrowing-Service-Client/img/twitter-logo.9ddf6437.svg"
+  },
+  {
+    "revision": "555e81cdc5d6852ee765170074d50223",
+    "url": "/Borrowing-Service-Client/img/twitter-logo-white.555e81cd.svg"
+  },
+  {
+    "revision": "8bb3107ba87486ceaf34db05bae8bebe",
+    "url": "/Borrowing-Service-Client/img/youtube-logo.8bb3107b.svg"
+  },
+  {
+    "revision": "f9fa404c185bcee62119604b280e3fb9",
+    "url": "/Borrowing-Service-Client/img/youtube-logo-white.f9fa404c.svg"
+  },
+  {
+    "revision": "e31e4d128b087f9625ba959a38b15df2",
+    "url": "/Borrowing-Service-Client/img/refresh.e31e4d12.png"
+  },
+  {
+    "revision": "9a4a37aface59d49bf02b604501d5c20",
+    "url": "/Borrowing-Service-Client/img/logo.9a4a37af.png"
+  },
+  {
+    "revision": "7e1595b75dc02dd4d0867bb59d2cabc9",
+    "url": "/Borrowing-Service-Client/index.html"
+  },
+  {
+    "revision": "b67a979490f83f0760cb",
+    "url": "/Borrowing-Service-Client/css/about.1d72de18.css"
+  }
+];
